c03bbe072ee6e02a2837c569c53de7edd0364f9f
wangjiawen2 <tecwang@126.com>
2024-08-21 21:22:42 +0800

    iteration

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,4 +1,5 @@
-package org.example
+
+import java.io.File
 
 object t {
   def main(args: Array[String]): Unit = {
@@ -7,6 +8,8 @@ object t {
 }
 
 object TestMethod {
+
+  //  自定义方法
   private def add(a:Int, b:Int):Int = {
     a + b
   }
@@ -20,9 +23,23 @@ object TestMethod {
     }
   }
 
+  def listFiles (path:String): Unit = {
+    val file = new File(path)
+    if (file.isFile) {
+      println(file.getPath)
+    } else {
+      println(file.getPath)
+      val fs = file.list()
+      for (f <- fs) {
+        listFiles(path + "/" + f)
+      }
+    }
+  }
+
   def main(args: Array[String]):Unit = {
     println(add(1,2))
     sayHello()
     sayHelloV2("test")
+    listFiles("C:\\Users\\tecwang\\IdeaProjects\\scala\\src")
   }
 }

5bd66dc3e0b05baa568307982171522952b5f0ae
tecwang <tecwang@126.com>
2024-10-05 22:05:03 +0800

    20241005 优化mysql安装步骤，初学flink，跑通wordcount程序

---
+++ src/learn/src/main/java/base/flink/AFlinkWordCountDataSet.scala
@@ -0,0 +1,39 @@
+package base.flink
+
+//import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment, createTypeInformation}
+// 直接引入所有内容
+import org.apache.flink.api.scala._
+
+object AFlinkWordCountDataSet {
+    def main(args: Array[String]): Unit = {
+
+        val env = ExecutionEnvironment.getExecutionEnvironment
+
+        val lineDS:DataSet[String] = env.readTextFile("C:\\Users\\rumor\\.github\\bigdata\\data\\word.csv")
+
+        println("lineDS", lineDS)
+
+        // 语法会飘红，需要引入 createTypeInformation
+        val lineDSplited = lineDS.flatMap(item => {
+            val words = item.split(",")
+//            for (word <- words) println(word)
+            words
+        })
+
+        val lineDSToMap: DataSet[(String, Int)] = lineDSplited.map(item => (item, 1))
+
+        val lineDSGroupBy: GroupedDataSet[(String, Int)] = lineDSToMap.groupBy(fields = 0)
+
+        val lineDSumBy: AggregateDataSet[(String, Int)] = lineDSGroupBy.sum(field = 1)
+
+        lineDSumBy.print()
+        // 调用 print 方法时，一直在报错。
+            // No ExecutorFactory found to execute the application in Flink
+        // 查询多处均表示是缺少 flink-clients的原因，但是反复调整 pom 无果
+            // 于是，从网上下载 flink-clients 并配置项目依赖，就可以正常运行了。
+            // jar包下载地址: https://developer.aliyun.com/mvn/search
+
+//        env.execute()
+
+    }
+}
